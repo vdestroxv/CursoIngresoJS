1b7e1e6feb66bb481974348796b609f78ab21c9c
Sergio Barrios <60700413+vdestroxv@users.noreply.github.com>
2020-02-06 11:14:07 -0300

    ejercicio uno
    uso de alert, variables, valor literal

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,18 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+
 function mostrar()
 {
-	alert("ok");
+	var primerprecio;
+	var segundoprecio;
+	var apellido="nadie";
+
+	apellido="Barrios";
+	alert(apellido)
+	//alert("tal cual");valor literal
+	//alert(apellido);valor variable
+	//alert();imprime vacio
+	//alert(preciopri);si no existe variable no hace nada
+
+
 }
 
