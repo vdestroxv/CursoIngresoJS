b7deb1faa07264730215c47a7e90b8467a55ef73
alumno <alumno>
2020-02-13 09:44:06 -0300

    git push

--- CursoDeGit.js
+++ CursoDeGit.js
@@ -2,4 +2,4 @@ git clone https://github.com/vdestroxv/CursoIngresoJS.git
 
 git add .  //trabaja sobre el local
 git commint -m //añade un mensaje en el commit
-
+git push// carga tu trabajo en gihub
