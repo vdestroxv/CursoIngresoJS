14096141b6a2fc994742bcaa1f212ecfce774d80
Sergio Barrios <60700413+vdestroxv@users.noreply.github.com>
2020-02-27 23:08:45 -0300

    recu_parcial ejercicio 9
    falta terminar el ultimo punto

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -1,33 +1,77 @@
 function mostrar()
-{
+{	
+	var respuesta;
 	var nombre;
-	var sexo;
 	var edad;
+	var sexo;
 	var notafinal;
-	var contador;
 
-	nombre = prompt("ingrese un nombre");
-	while(isNaN(nombre))
+	respuesta="si";
 
-	edad = prompt("ingrese la edad");
-	edad = parseInt(edad);
-	while (isNaN(edad<0 || edad>110))
+	while(respuesta!="no")
 	{
-		console.log(edad);
+		nombre=prompt("ingrese un nombre");
+		edad=prompt("ingrese la edad");
+		edad=parseInt(edad);
+		while(isNaN(edad)|| edad<0 || edad>110)
+		{
+			edad=prompt("ingrese una edad valida");
+			edad=parseInt(edad);
+		}
+		sexo = prompt("ingrese el sexo ");
+		while(!isNaN(sexo) || sexo !="f" && sexo !="m")
+		{
+			sexo=prompt("ingrese el sexo correctamente (f o m)")
+		}
+		notafinal = prompt("ingrese la nota final");
+		notafinal = parseInt(notafinal);
+		while (isNaN(notafinal)|| notafinal<0 || notafinal>10)
+		{
+			notafinal=prompt("ingrese una nota valida");
+			notafinal=parseInt(notafinal);
+		
+		}
+		if (sexo=="m")
+		{
+			if(notafinal>5)
+			{
+				totalmasculinosaprobados=totalmasculinosaprobados+1;
+			}
+		}
+		if(edad<13)
+		{
+			totalnotasmenores=totalnotasmenores+notafinal;
+			cantidadmenores=cantidadmenores+1;
+		}
+		else
+		{
+			if(edad>12 && edad<18)
+			{
+				totalnotasadolecentes=totalnotasadolecentes+notafinal;
+				cantidadadolecentes=cantidadadolecentes+1;
+			}
+			else
+			{
+				if(edad>17)
+				{
+					totalnotasmayores=totalnotasmayores+notafinal;
+					cantidadmayores=+1;
+				}
+			}
+		}
+		//total masculino aprobados
+		//notas/contador menores
+		//notas/contador adolecentes
+		//notas/contador mayores
+		//notas/ contador m y f
+		
+		respuesta=prompt("desea ingresar otro alumno");
 	}
-
-	sexo=prompt("ingrese el sexo")
-	while(isNaN(sexo !="m" && sexo !="f"))
-	{
-		console.log(sexo);
-	}
-	notafinal = prompt("ingrese la nota final");
-	notafinal = parseInt(notafinal);
-	while(isNaN(notafinal))
-	{
-		console.log(notafinal);
-	}
-
-
-
+	promediommenores=totalnotasmenores/cantidadmenores;
+	promedioadolecentes=totalnotasadolecentes/cantidadadolecentes;
+	promediomayores=totalnotasmayores/cantidadmayores;
+	console.log
+	console.log
+	console.log
+	
 }
